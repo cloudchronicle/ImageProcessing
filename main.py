6d85557082b5f7c7e7e315983275c6183854556c
jinchang9292 <jinchang9292@gmail.com>
2023-03-10 10:54:18 +0900

    Mast Commit 2 Gray Scale 효과 추가 RGB2GRAY

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import cv2
 
+# 영상 소스 열기
 src1 = cv2.imread('c:\images\picture13.jpg')
 
+### 영상 처리 알고리즘을 구현 ###
+dst1 = cv2.cvtColor(src1, cv2.COLOR_RGB2GRAY)
 
+
+
+
+
+#################
+
+
+# 영상 디스플레이
 cv2.imshow('src1', src1)
+cv2.imshow('dst1', dst1)
 
 
 
 
-
+#마무리
 cv2.waitKey(0)
 cv2.destroyAllWindows()
